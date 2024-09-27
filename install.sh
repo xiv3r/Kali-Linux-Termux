a5c930326598c314fd80b7969ffdefdf5df76595
x!v3r <117867334+xiv3r@users.noreply.github.com>
2024-09-28 05:12:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 apt update ; apt upgrade -y ; apt install wget -y
+echo "clear ; nh" >$PREFIX/etc/bash.bashrc
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod +x install-nethunter-termux
-echo "clear ; nh" >$PREFIX/etc/bash.bashrc
-./install-nethunter-termux
+
+
