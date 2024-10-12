a4fd93f8af686c1684473db1225a9b30d3c96285
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-13 06:25:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ apt upgrade -y ; apt install wget -y
 echo "clear ; nh" >$PREFIX/etc/bash.bashrc
 wget -O install-nethunter-termux https://raw.githubusercontent.com/xiv3r/Kali-Linux-Termux/refs/heads/main/setup
 chmod +x install-nethunter-termux
+./install-nethunter-termux
 
 
