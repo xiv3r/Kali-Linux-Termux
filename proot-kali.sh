aed57c3fe83e85d304d7bac5bfb55af9b1b72561
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-18 15:13:13 +0800

    Update proot-kali.sh

--- proot-kali.sh
+++ proot-kali.sh
@@ -3,17 +3,14 @@
 ###
 apt update && apt install curl gnupg -y
 ###
-echo "
-# deb https://deb.debian.org/debain stable main
-
-deb https://kali.download/kali kali-rolling main contrib non-free non-free-firmware
-" > /etc/apt/sources.list
-###
-echo "Installing kali gpg keyring..."
-###
 curl -fsSL https://archive.kali.org/archive-key.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/kali-archive-keyring.gpg
 ###
-echo "update and upgrading kali..."
+cp /etc/apt/trusted.gpg.d/kali-archive-keyring.gpg /usr/share/keyrings/
+###
+echo "
+# deb https://deb.debian.org/debain stable main
+deb [signed-by="/usr/share/keyrings/kali-archive-keyring.gpg"] https://kali.download/kali kali-rolling main contrib non-free non-free-firmware
+" > /etc/apt/sources.list
 ###
 apt update && apt upgrade -y
 
