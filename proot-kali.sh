858f0f9dcef56009cc66bba0023efa37d5275bd5
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-18 17:27:13 +0800

    Update proot-kali.sh

--- proot-kali.sh
+++ proot-kali.sh
@@ -3,6 +3,8 @@
 ###
 echo PS1='${debian_chroot:+($debian_chroot)}\u@kali:\w\$ ' >> /etc/bash.bashrc
 ###
+source /etc/bash.bashrc
+###
 apt update && apt install curl gnupg -y
 ###
 wget -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc https://archive.kali.org/archive-key.asc
