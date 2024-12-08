4b7059bfef0f3180f9e04f54ff8450bf458c3e15
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-08 18:57:05 +0800

    Delete adb.sh

--- adb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo apt install adb fastboot -y
-adb devices && adb shell /system/bin/device_config set_sync_disabled_for_tests persistent && adb shell /system/bin/device_config put activity_manager max_phantom_processes 2147483647 && adb shell settings put global settings_enable_monitor_phantom_procs false
